08d4e7c35243240c93d178a2523afb8582589fb4
Vedant Agrawal <96226581+vedantagrawal524@users.noreply.github.com>
2023-09-10 11:38:47 +0530

    meal traits added

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,8 +60,24 @@ class MealItem extends StatelessWidget {
                         ),
                       ),
                       const SizedBox(height: 12),
-                      const Row(
-                        children: [],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          MealItemTrait(
+                            icon: Icons.schedule,
+                            label: '${meal.duration} min',
+                          ),
+                          const SizedBox(width: 12),
+                          MealItemTrait(
+                            icon: Icons.work,
+                            label: complexityText,
+                          ),
+                          const SizedBox(width: 12),
+                          MealItemTrait(
+                            icon: Icons.currency_rupee_rounded,
+                            label: affordabilityText,
+                          ),
+                        ],
                       ),
                     ],
                   )),
