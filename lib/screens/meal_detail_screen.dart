7a2cfd9aad0d21e5049916f8d4e277a55907ef16
Vedant Agrawal <96226581+vedantagrawal524@users.noreply.github.com>
2023-09-12 01:11:06 +0530

    Icon and Name Added

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -36,7 +36,7 @@ class MealDetailScreen extends ConsumerWidget {
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation) {
                 return RotationTransition(
-                  turns: Tween<double>(begin: 0.8, end: 1).animate(animation),
+                  turns: Tween<double>(begin: 0.9, end: 1).animate(animation),
                   child: child,
                 );
               },
